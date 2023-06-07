ce1b517a2a5baa7db2e3640ee9c2f4f1610824b8
Marco Sciaini <marco@kaldera.dev>
2023-06-07 22:19:43 +0200

    init

---
+++ ShinyModule.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(ctmm)
+library(sf)
+library(glue)
+library(dplyr)
+library(purrr)
+library(mapview)
+library(leaflet)
+
+shinyModuleUserInterface <- function(id, label) {
+  ns <- NS(id) ## all IDs of UI functions need to be wrapped in ns()
+  tagList(
+    titlePanel("Outlier detection"),
+    fluidRow(
+      column(4,
+             wellPanel(
+               sliderInput(
+                 ns("isopleth_levels"),
+                 "Isopleth levels:",
+                 min = 0.01, max = .99, value = .95
+               ),
+             )
+      )
+    ),
+    leafletOutput(ns("map"))
+  )
+}
+
+shinyModule <- function(input, output, session, data){ ## The parameter "data" is reserved for the data object passed on from the previous app
+  ns <- session$ns ## all IDs of UI functions need to be wrapped in ns()
+  
+  hr <- akde(data[[1]], data[[2]])
+  
+  output$map <- renderLeaflet({
+  
+    akde_sf <- map(hr, ~ sf::st_as_sf(ctmm::SpatialPolygonsDataFrame.UD(.x, level.UD = input$isopleth_levels))) |> bind_rows()
+    m <- mapview(akde_sf)
+    # export as geopackage
+    akde_sf |> 
+      sf::st_write(appArtifactPath(glue::glue("homerange.gpkg")))
+    m@map
+  })
+  
+  return(reactive({ 
+    hr
+  })) ## if data are not modified, the unmodified input data must be returned
+}
